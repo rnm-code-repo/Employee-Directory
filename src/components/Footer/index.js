f3fd9a3c0af8f4dbedbbce619366f76aaa79de9c
Rekha Ballur <rnmadalli@gmail.com>
2020-04-11 20:33:07 -0400

    Initia files

---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+     &#169; Employees Directory 2020
+    </footer>
+  );
+}
+export default Footer;
