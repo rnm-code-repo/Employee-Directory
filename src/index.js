19759f66e75c3ea6ed1a5d811a46f43b5afdecca
Rekha Ballur <rnmadalli@gmail.com>
2020-04-11 21:13:06 -0400

    Updates

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import "./index.css";
-import '../node_modules/font-awesome/css/font-awesome.min.css'; 
-
-
-ReactDOM.render(<App />, document.getElementById("root"));
--- src/utils/API.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-export default {
-  getEmployees: function() {
-    return axios.get('https://randomuser.me/api/?nat=us&results=200');
-  }
-}
